52c34a6813deb4562e7e02ebca50fd856eb4eb60
breezing <542538469@qq.com>
2025-09-19 05:59:09 +0800

    sample by probability

--- players/policy_player.py
+++ players/policy_player.py
@@ -1,6 +1,9 @@
 # %%
 # 只通过策略网络来进行落子
+import torch
 from torch import Tensor
+import torch.nn.functional as F
+
 from .base_player import BasePlayer
 from chess.board import Board
 from chess.define import LEGAL_MOVES, MOVE_TO_INDEX, Move, StateTensor
@@ -9,8 +12,9 @@ from net.policy_net import PolicyNet
 
 
 class PolicyPlayer(BasePlayer):
-  def __init__(self, name: str, model: PolicyNet):
+  def __init__(self, name: str, model: PolicyNet, is_selfplay: bool = True):
     super().__init__(name)
+    self.is_selfplay = is_selfplay  # 自我对弈时候按照概率采样 否则选取概率最大值
     self.model = model
     self.model.to('cpu')
     # TODO 是不是在gpu上推断更快
@@ -27,7 +31,11 @@ class PolicyPlayer(BasePlayer):
 
     legal_moves = board.available_moves()
     legal_moves_ids = [MOVE_TO_INDEX[m] for m in legal_moves]
-
     policy_logits = policy_logits[legal_moves_ids]
-    index = policy_logits.argmax(dim=-1)
+
+    if self.is_selfplay:
+      probs = F.softmax(policy_logits, dim=-1)
+      index = torch.multinomial(probs, num_samples=1, replacement=True)
+    else:
+      index = policy_logits.argmax(dim=-1)
     return legal_moves[index]
--- selfplay.py
+++ selfplay.py
@@ -16,9 +16,9 @@ model = PolicyNet.load_from_checkpoint(checkpoint_path)
 red_player = PolicyPlayer("红方", model=model)
 black_player = PolicyPlayer("黑方", model=model)
 game = Game(red_player, black_player, debug=True)
-game.start_play_loop()
 
 # %%
+game.start_play_loop()
 movelist = game.movelist
 
 
@@ -27,3 +27,5 @@ from chess.utils import generate_board_images
 from utils import show_images_in_slider
 images = generate_board_images(movelist, show_last_pos=True)
 show_images_in_slider(images)
+
+# %%
