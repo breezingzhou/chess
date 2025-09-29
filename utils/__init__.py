c85a6c8eb043ada2ec000ca30e6c8643f2aa9fbe
breezing <542538469@qq.com>
2025-09-30 02:58:58 +0800

    rename file

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 from .common import WORKSPACE
-from .show_in_slider import show_images_in_slider
+from .display import show_images_in_slider
 from .utils import setup_logging, timer
 from .self_play import collect_selfplay_data
